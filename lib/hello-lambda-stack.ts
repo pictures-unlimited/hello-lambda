ce7ebc482ff2fd8e03cbbfef9678e08ea28eea06
Juha Luoto <jtluoto@gmail.com>
2025-01-10 15:09:58 +0200

    Updated lambda function

--- lib/hello-lambda-stack.ts
+++ lib/hello-lambda-stack.ts
@@ -1,16 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as apigw from "aws-cdk-lib/aws-apigateway";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as path from 'node:path';
 
 export class HelloLambdaStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: cdk.StackProps){
+    super(scope, id, props)
+    const fn = new lambda.Function(this, 'MyFunction', {
+      runtime: lambda.Runtime.NODEJS_LATEST,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler')),
+    });
 
-    // The code that defines your stack goes here
+    const endpoint = new apigw.LambdaRestApi(this, `ApiGwEndpoint`, {
+      handler: fn,
+      restApiName: `HelloApi`,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
